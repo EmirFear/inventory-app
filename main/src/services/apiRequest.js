2cdf6cf42260c4a5737c37af87c1fddbec983d27
Emir <emir.ser32@gmail.com>
2024-05-28 20:36:00 +0300

    api request starter

--- main/src/services/apiRequest.js
+++ main/src/services/apiRequest.js
@@ -1,1 +1,15 @@
-const login = async() => {}
+import axios from "axios"
+
+
+export const login = async(userData) => {
+    const BASE_URL = "https://10157.fullstack.clarusway.com" 
+    const data = await axios(`${BASE_URL}/auth/login`, userData)
+
+    try {
+        const data = await axios(`${BASE_URL}/auth/login`, userData)
+    } catch {
+        
+    }
+
+    
+}
