5132010635088e68703f5f6b5c126a6b36da3d64
Emir <emir.ser32@gmail.com>
2024-06-01 17:57:07 +0300

    fetch start

--- main/src/services/apiRequest.js
+++ main/src/services/apiRequest.js
@@ -1,12 +1,21 @@
 import axios from "axios"
-
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify"
+import { fetchStart } from "../features/authSlice"
+import { useDispatch } from "react-redux"
 
 export const login = async(userData) => {
-    
+    const dispatch = useDispatch()
+
+    dispatch(fetchStart())    
 
     try {
-        const data = await axios(`${process.env.REACT_APP_BASE_URL}/auth/login`, userData)
-    } catch {
+        const {data} = await axios.post(`${process.env.REACT_APP_BASE_URL}/auth/login`, userData)
+        toastSuccessNotify("Login işlemi başarılı")
+        console.log(data);
+        return data
+    } catch (error){
+        console.log(error);
+        toastErrorNotify("Login başarısız oldu")
 
     }
 
